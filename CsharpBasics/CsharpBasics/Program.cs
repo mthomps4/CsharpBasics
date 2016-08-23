0ab4cb1af1e974790e996c6334619a959a307cc8
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-08-23 13:26:15 -0400

    Basic app structure complete. User enteres minutes and types "quit" to end.

--- CsharpBasics/CsharpBasics/Program.cs
+++ CsharpBasics/CsharpBasics/Program.cs
@@ -4,34 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CsharpBasics //Mailing Address example Namespace (State) class (City) Method (street) System.Console.Write 
+namespace CsharpBasics //Mailing Address example Namespace (State) class (City) Method (street)  ie: System.Console.Write 
 {
     //Command Line Application
     class Program
     { //Everything is in a class 
         static void Main()//Method .. First one always ran is called "Main" all other methods can be named what ever
         {
-            //Prompt user for minutes exercised 
             //Use .net framework 
             //Terminal csc Program.cs creates Program.exe when not using VS 
 
-            Console.Write("Enter how many minutes you exercised: ");
+            int runningTotal = 0;
 
-            string entry = Console.ReadLine();
+            //Repeat until user ends        
+            bool keepGoing = true;
+            while (keepGoing) {
 
+                //Prompt user for minutes exercised 
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+                string entry = Console.ReadLine().ToLower();
 
+                if (entry == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    int minutes = int.Parse(entry);
+                    //Add to total 
+                    runningTotal = runningTotal + minutes;
 
-            //Add to total 
-            //Display to screen 
-            Console.WriteLine("You've exercised " + entry + " minutes.");
+                    //Display to screen 
+                    Console.WriteLine("You've exercised " + runningTotal + " minutes.");
+                }
+               }
 
-            //Repeat until user ends 
-
-            Console.WriteLine("Some other information here.");
+            Console.WriteLine("Great Job!"); 
+            
+            
+            
             //Keep console window open in debug mode
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
             //Look into Mono C# REPL when not on Windows VS
 
         }
